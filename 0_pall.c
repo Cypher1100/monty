d5b160de7dc5f29ad6e76ebc00b51abdb0cf73cf
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-14 10:07:40 -0800

    task 0

---
+++ 0_pall.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+* _pall - function prints everything in the stack
+* @head: pointer to the head of the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void _pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
+/**
+* f_swap - function that swaps the top two elements of the stack
+* @head: head of stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void _swap(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(parabus.file);
+		free(parabus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n;
+	h->n = h->next->n;
+	h->next->n = temp;
+}
---
+++ 0_push.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+* _push - function adds a newnode to the stack
+* @head: double pointer to the head of the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void _push(stack_t **head, unsigned int counter)
+{
+	int i, idx = 0, flag = 0;
+
+	if (parabus.arg)
+	{
+		if (parabus.arg[0] == '-')
+			idx++;
+		for (; parabus.arg[idx] != '\0'; idx++)
+		{
+			if (parabus.arg[idx] > 57 || parabus.arg[idx] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(parabus.file);
+			free(parabus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(parabus.file);
+		free(parabus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	i = atoi(bus.arg);
+	if (parabus.lifi == 0)
+		addnode(head, i);
+	else
+		addqueue(head, i);
+}
